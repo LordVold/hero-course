6f529e353b7b54b7a2b5f8f3a7c96dcc4185e3a4
Lord Volodymyr <47691611+LordVold@users.noreply.github.com>
2020-04-17 14:23:23 +0300

    '17.04'

---
+++ js/codeWars.js
@@ -0,0 +1,23 @@
+// ~~~ #1 ~~~ 6 kyu ~~~ I need more speed! ~~~
+function reverse(arr) {
+  let revArr = [];
+  let j = 0;
+  for (let i = 1; i <= arr.length; i++) {
+    revArr[j] = arr[arr.length - i];
+    j++
+  }
+return revArr //  = arr.reverse();
+}
+/* function reverse(arr) {
+  for (var i = 0; i <= Math.floor((arr.length - 1) / 2); i++) {
+    let elem = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = elem;
+  }
+  return arr;
+} */
+console.log("[]", reverse([1, 2, 3, 4, 5]));
+// console.log("[]", reverse(['hello', 'world', 'how', 'are', 'you', '?']));
+// console.log("{}", reverse([{a: 5}, {b: 4}]));
+// /|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|
+// ~~~ #2 ~~~ 6 kyu ~~~ I need more speed! ~~~
---
+++ js/task.js
@@ -0,0 +1,42 @@
+// ~~~~~~~~   task #1   ~~~~~~~~~
+let strOne = 'There is a string one';
+// console.log(String.fromCharCode(66));
+// console.log(strOne.charCodeAt(3));
+//___________________________________________________________________________
+let str = 'some test string for some';
+let first_lastLetters = str[0] + str[str.length - 1];
+let first_lastLettersMod = str.slice(0, 1) + str.slice(-1);
+let first_lastLettersUp = str.slice(0, 1).toUpperCase() + str.slice(1, -1) + str.slice(-1).toUpperCase();
+let position = str.indexOf(' ', str.indexOf(' ') + 1);
+let from5Long4 = str.slice(4, 8);
+let from5to9 = str.slice(4, 10);
+let withoutLast6 = str.slice(0, -6);
+let a = 20, b = 20;
+let fromNumberToString = '' + a + b;
+// ~~~ Math
+Math.round();
+Math.floor();
+Math.ceil();
+(1.234).toFixed();
+// ~~~
+const PI = Math.PI.toFixed(2);
+let minNum = Math.min(15, 11, 16, 12, 51, 13, 51);
+let hundred = '100$';
+hundred = parseInt(hundred);
+//___________________________________________________________________________
+let obj1 = {
+  product: 'iphone',
+}
+obj1.price = 1000;
+obj1.currency = 'dollar';
+obj1.details = {};
+obj1.details.model;
+obj1.details.model = 'lid';
+obj1.details.color;
+obj1.details.color = 'red';
+// console.log("obj1", obj1)
+// /|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|
+// ~~~~~~~~   task #2   ~~~~~~~~~
+
+// /|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|
+// ~~~~~~~~   task #3   ~~~~~~~~~
